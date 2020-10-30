7ce33507ad417ad36863370269dca1b7b5892fdf
ossup0v@gmail.com <ossup0v@gmail.com>
2020-10-30 23:28:33 +0000

    init commit

---
+++ RabbitMQ.Consumer/RabbitMQ.Consumer/Program.cs
@@ -0,0 +1,47 @@
+﻿using Newtonsoft.Json;
+using RabbitMQ.Client;
+using RabbitMQ.Client.Events;
+using System;
+using System.Text;
+
+namespace RabbitMQ.Consumer
+{
+  static class Program
+  {
+    private static void Main(string[] args)
+    {
+      var factory = new ConnectionFactory
+      {
+        Uri = new Uri("amqp://guest:guest@localhost:5672")
+      };
+
+      using (var conn = factory.CreateConnection())
+      using (var channel = conn.CreateModel())
+      {
+        channel.QueueDeclare(
+            queue: "demo-queue"
+          , durable: false
+          , exclusive: false
+          , autoDelete: false
+          , arguments: null);
+
+        var consumer = new EventingBasicConsumer(channel);
+
+        consumer.Received += (sender, e) =>
+        {
+          var body = e.Body.ToArray();
+          var message = Encoding.UTF8.GetString(body);
+          Console.WriteLine(message);
+        };
+
+        channel.BasicConsume(
+            queue: "demo-queue"
+          , autoAck: true
+          , consumer: consumer);
+
+        Console.WriteLine($"Start listening..");
+        Console.ReadLine();
+      }
+    }
+  }
+}
---
+++ RabbitMQ.Producer/RabbitMQ.Producer/Program.cs
@@ -0,0 +1,37 @@
+﻿using Newtonsoft.Json;
+using RabbitMQ.Client;
+using System;
+using System.Text;
+
+namespace RabbitMQ.Producer
+{
+  static class Program
+  {
+    private static void Main(string[] args)
+    {
+      var factory = new ConnectionFactory
+      {
+        Uri = new Uri("amqp://guest:guest@localhost:5672")
+      };
+
+      using (var conn = factory.CreateConnection())
+      using (var channel = conn.CreateModel())
+      {
+        channel.QueueDeclare(
+            queue: "demo-queue"
+          , durable: false
+          , exclusive: false
+          , autoDelete: false
+          , arguments: null);
+        var message = new { Name = "Producer", Message = "Hello!!" };
+
+        var body = Encoding.UTF8.GetBytes(JsonConvert.SerializeObject(message));
+
+        channel.BasicPublish("", "demo-queue", null, body);
+        Console.WriteLine($"Message {message.ToString()} was seneded");
+        Console.ReadLine();
+      }
+
+    }
+  }
+}
